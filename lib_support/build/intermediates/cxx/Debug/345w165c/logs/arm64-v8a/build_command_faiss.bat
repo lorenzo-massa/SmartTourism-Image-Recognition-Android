3609eb3ded2508f5f8610b82493fa579e761f074
Lorenzo Massa <lorenzo.massa6@studio.unibo.it>
2023-08-10 14:22:43 +0200

    opecv update

--- lib_support/build/intermediates/cxx/Debug/345w165c/logs/arm64-v8a/build_command_faiss.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Lorenzo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Lorenzo\\StudioProjects\\SmartTourism v2\\lib_support\\.cxx\\Debug\\345w165c\\arm64-v8a" ^
-  faiss
--- lib_support/build/intermediates/cxx/Debug/345w165c/logs/x86_64/build_command_faiss.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Lorenzo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Lorenzo\\StudioProjects\\SmartTourism v2\\lib_support\\.cxx\\Debug\\345w165c\\x86_64" ^
-  faiss
