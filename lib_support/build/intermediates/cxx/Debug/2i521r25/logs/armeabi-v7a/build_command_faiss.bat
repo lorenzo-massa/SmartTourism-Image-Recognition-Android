48e7569effc223d050b1d43fd1319039dc5b58ce
Marco Bertini <marco.bertini@unifi.it>
2023-10-18 15:16:12 +0200

    GIT: eliminate temporary build files from repo

--- lib_support/build/intermediates/cxx/Debug/2i521r25/logs/armeabi-v7a/build_command_faiss.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Lorenzo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Lorenzo\\StudioProjects\\SmartTourism v2\\lib_support\\.cxx\\Debug\\2i521r25\\armeabi-v7a" ^
-  faiss
--- opencv/build/intermediates/cxx/Debug/6c701vse/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\Lorenzo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\Lorenzo\\StudioProjects\\SmartTourism v2\\opencv\\.cxx\\Debug\\6c701vse\\arm64-v8a" ^
-  opencv_jni_shared
