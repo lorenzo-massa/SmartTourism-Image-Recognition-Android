a59b6675a41c916a6dc95b2f7ee48ef7ea8d5348
Lorenzo Massa <lorenzo.massa6@studio.unibo.it>
2024-02-01 09:52:01 +0100

    FIX: guides and categories paths updated

--- copy_guides.py
+++ copy_guides.py
@@ -13,18 +13,18 @@ guide_name = ap.parse_args().guides
 
 if guide_name:
     print('[INFO]: Using the guides for: ' + guide_name)
-    pathGuides = "models/src/main/assets/guides/" + guide_name
-    pathCategories = "models/src/main/assets/categories/" + guide_name
+    sourceGuides = "Python/guides/" + guide_name
+    sourceCategories = "Python/categories/" + guide_name
     # Check if the guide path exists
-    if not os.path.exists(pathGuides):
-        print("\n[ERROR]: The guides path " + pathGuides + " does not exist")
+    if not os.path.exists(sourceGuides):
+        print("\n[ERROR]: The guides path " + sourceGuides + " does not exist")
         exit(1)
     # Check if the categories path exists
-    if not os.path.exists(pathCategories):
-        print("\n[ERROR]: The categories path " + pathCategories + " does not exist")
+    if not os.path.exists(sourceCategories):
+        print("\n[ERROR]: The categories path " + sourceCategories + " does not exist")
         exit(1)
 
-    print("\n[INFO]: Using the guides in " + pathGuides)
+    print("\n[INFO]: Using the guides in " + sourceGuides)
     # Delete old guides
     if os.path.exists("models/src/main/assets/currentGuide"):
         print("[INFO]: Deleting old guides")
@@ -32,26 +32,26 @@ if guide_name:
     # Create new guides folder
     os.makedirs("models/src/main/assets/currentGuide")
     # Copy all the files from the provided path in models/src/main/assets/currentGuide
-    print("[INFO]: Copying guides from " + pathGuides + " in " + os.path.realpath('models/src/main/assets/currentGuide'))
-    dirs = os.listdir(pathGuides)
+    print("[INFO]: Copying guides from " + sourceGuides + " in " + os.path.realpath('models/src/main/assets/currentGuide'))
+    dirs = os.listdir(sourceGuides)
     print("[INFO]: Found " + str(len(dirs)) + " guides")
     for d in dirs:
         if has_trailing_spaces(d):
             print("[WARN]: Removing trailing spaces from '" + d + "'")
-            source_dir = pathGuides + "/" + d
+            source_dir = sourceGuides + "/" + d
             os.rename(source_dir, source_dir.rstrip())
             d = d.rstrip()
-        shutil.copytree(pathGuides + "/" + d, "models/src/main/assets/currentGuide/" + d)
-    print("\n[INFO]: Using the categories in " + pathCategories)
+        shutil.copytree(sourceGuides + "/" + d, "models/src/main/assets/currentGuide/" + d)
+    print("\n[INFO]: Using the categories in " + sourceCategories)
     # Delete old categories
     if os.path.exists("models/src/main/assets/currentCategories"):
         print("[INFO]: Deleting old categories")
         shutil.rmtree("models/src/main/assets/currentCategories")
     # Create new categories folder
     # os.makedirs("models/src/main/assets/currentCategories")  # XXX seems there's no need to create it as the copy will do it
-    # Copy all the files from pathCategories in models/src/main/assets/currentCategories
-    print("[INFO]: Copying categories from " + pathCategories + " in " + os.path.realpath('models/src/main/assets/currentCategories'))
-    shutil.copytree(pathCategories + "/", "models/src/main/assets/currentCategories/")
+    # Copy all the files from sourceCategories in models/src/main/assets/currentCategories
+    print("[INFO]: Copying categories from " + sourceCategories + " in " + os.path.realpath('models/src/main/assets/currentCategories'))
+    shutil.copytree(sourceCategories + "/", "models/src/main/assets/currentCategories/")
     print("\n[INFO]: Guides copied in " + os.path.realpath('models/src/main/assets/currentGuide'))
     print("[INFO]: Categories copied in " + os.path.realpath('models/src/main/assets/currentCategories'))
 else:
